5527687bbd437a32fbf5bee79dcda95c841695a2
wertrain <wertrain@gmail.com>
2020-05-28 00:06:41 +0900

    ユーティリティ追加

--- js/common/common.js
+++ js/common/common.js
@@ -44,4 +44,18 @@ var ShimaMeloPlayer = {};
     drawFunc(context);
     return canvas;
   };
+
+  ShimaMeloPlayer.Common.convertDataURIToBinary = function(dataURI) {
+    const BASE64_MARKER = ';base64,';
+    const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    const base64 = dataURI.substring(base64Index);
+    const raw = window.atob(base64);
+    const rawLength = raw.length;
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+  
+    for(i = 0; i < rawLength; i++) {
+      array[i] = raw.charCodeAt(i);
+    }
+    return array;
+  }
 }
